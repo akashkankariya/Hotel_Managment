378b99cc9b27fd5dbd5f3ae174971a57d5c4ffa4
AKASH <akashkankariya10@gmail.com>
2018-09-06 23:30:21 +0530

    ok

---
+++ HM/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Marina Mirage</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="img/logo.png" rel="icon">
+</head>
+<body>
+	<div id="full">
+		<div style="background-image: url('img/fp.jpg');width: 100%;height: 450px;">
+		<div id="header">
+			<div id="logo">
+				<h2><font color="white">Marina Mirage</font></h2>
+			</div>
+			<div id="nav">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Login</a></li>
+					<li><a href="#">Booking</a></li>
+					<li><a href="#">Info</a></li>
+					<li><a href="#">Help</a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="banner"></div>
+		<div id="f1">
+			<center><table>
+				<tr>
+					<th width="20%" height="50px">Destination</th>
+					<th width="20%" height="50px">Check in date</th>
+					<th width="20%" height="50px">Check out Date</th>
+					<th width="20%" height="50px">Room</th>
+					<td rowspan="2"><input type="submit" name=sub" value="Check"></td>
+				</tr>
+				<tr>
+					<td width="20%" height="50px"><center><input type="text" name="d1" placeholder="Enter Destination"></center></td>
+					<td width="20%" height="50px"><center><input type="date" name="ci"></center></td>
+					<td width="20%" height="50px"><center><input type="date" name="co"></center></td>
+					<td width="20%" height="50px">
+						<center><select>
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
+						</select></center>
+					</td>
+				</tr>
+			</table></center>
+		</div>
+		<div id="welcome">
+			<h1 align="center">Welcome to Marina Mirage</h1>
+			<center><font size="4">Isme sab karchra dal na hai.</center>
+		</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ HM/r1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Marina Mirage</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="img/logo.png" rel="icon">
+</head>
+<body>
+	<div id="full">
+		<div style="background-image: url('img/fp.jpg');width: 100%;height: 450px;">
+		<div id="header">
+			<div id="logo">
+				<h2><font color="white">Marina Mirage</font></h2>
+			</div>
+			<div id="nav">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Login</a></li>
+					<li><a href="#">Booking</a></li>
+					<li><a href="#">Info</a></li>
+					<li><a href="#">Help</a></li>
+				</ul>
+			</div>
+		</div>
+		</div>
+	</div>
+</body>
+</html>
